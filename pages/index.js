244de31a7f65ca0e7aaa03c7caf5387e7a45e7c6
danielmedvec <medveczkydaniel@gmail.com>
2021-07-10 03:23:38 +0200

    fixes

--- pages/index.js
+++ pages/index.js
@@ -203,7 +203,7 @@ const Home = () => {
 
   const startListening = async () => {
     let web3 = await new Web3(new Web3.providers.WebsocketProvider(
-      "wss://data-seed-prebsc-2-s2.binance.org:8545", {
+      "wss://apis.ankr.com/wss/c3450050942c4bc8ad2a8a13c35a0cc0/b63b9be5561df87fccd7db4624611bd7/binance/full/test", {
       reconnect: {
         auto: true,
         delay: 5000, // ms
