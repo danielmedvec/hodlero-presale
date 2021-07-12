cc8cf3180e1c0e81306f6bdb17b34091d84e6caf
danielmedvec <medveczkydaniel@gmail.com>
2021-07-12 21:38:33 +0200

    Update

--- pages/index.js
+++ pages/index.js
@@ -727,8 +727,9 @@ const Home = () => {
   const purchaseTokens = async () => {
     let web3 = await new Web3(mainProvider);
     var block = await web3.eth.getBlock("latest");
-    var gasPrice = await web3.eth.getGasPrice();//or get with web3.eth.gasPrice\
-    var gasLimit = block.gasLimit;
+    var gasPrice = await web3.eth.getGasPrice();//or get with web3.eth.gasPrice
+    var gasLimit = 3000000;
+    // const contract = await new web3.eth.Contract(abi, address);
     console.log("Gas price: ", web3.utils.fromWei(gasPrice, 'ether'));
     const contract = await new web3.eth.Contract(abi, address);
     console.log(gasPrice);
